460b72e430301358e1bb629a3dbc71bd6e1125e0
ajaya-work <ajayainfinitework@gmail.com>
2026-07-07 14:53:44 +0545

    component basic

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,21 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from './login/login';
 
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Login],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
- name = "Angular Tutorial";
- isAdmin = true;
- count = 5;
- price = 100.5;
- isLogin = true;
- title = signal("Welcome to Angular");
- getUser (){
-  return "John Doe";
- }
+
 }
