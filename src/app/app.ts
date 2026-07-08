424ce1d86522f1b6cdb431dbe45074616f1cc072
ajaya-work <ajayainfinitework@gmail.com>
2026-07-08 14:18:30 +0545

    different events

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,5 +9,31 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
- 
+ handleClick (){
+  console.log("Button clicked");
+ }
+
+ onTyping(event: any){
+  console.log("Typed", event.target.value);
+ }
+
+ onKeyUp(event: any){
+  console.log("Key up", event.key);
+ }
+
+ onHover(){
+  console.log("Mouse over");
+ }
+
+ onLeave(){
+  console.log("Mouse leave");
+ }
+
+ onBlur(){
+  console.log("Input lost focus");
+ }
+
+ onFocus(){
+  console.log("Input gained focus");
+ }
 }
