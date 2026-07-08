b1526104021b47d0bb6ad94dec01179438ac3820
ajaya-work <ajayainfinitework@gmail.com>
2026-07-08 11:10:34 +0545

    event binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Events } from './events/events';
 
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Events, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/events/events.ts
@@ -0,0 +1,28 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-events',
+  imports: [],
+  templateUrl: './events.html',
+  styleUrl: './events.scss',
+})
+export class Events {
+  count = signal(0);
+  increment() {
+    this.count.update((c) => c + 1);
+  }
+
+  showEvent(e:any){
+    console.log(e);
+  }
+
+  username = "";
+  updateUser(value: string) {
+    this.username = value;
+  }
+
+  handleSubmit(e: any) {
+    e.preventDefault();
+    console.log('Form submitted');
+  }
+}
