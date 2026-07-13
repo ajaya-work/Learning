21723e131c88fedcb61c33d5210b9351fe3116bb
ajaya-work <ajayainfinitework@gmail.com>
2026-07-13 18:21:26 +0545

    for loop

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,5 +9,14 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
- 
+ items = ['HTML','CSS','JavaScript', 'Angular'];
+ users =[{name: 'Ajaya', age: 24},{name: 'John', age: 30},{name: 'Alice', age: 18}]
+
+ fruit = ['Apple','Banana','Cherry','Date'];
+
+ removeItem(i: number){
+  this.fruit.splice(i,1);
+ }
+
+ numbers = [10,20,15,30,25];
 }
